bc1d43a848731e1966ad48b57738397d12e8c563
schultzy192 <darthsidious2011@gmail.com>
2017-09-18 08:57:08 +1200

    Corrected login page styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,42 @@
 $(document).ready(function() {
 
+    var $poster = $('.login-container'),
+        $shine = $('.shine'),
+        $layer = $('div[class*="layer-"]'),
+        w = $(window).width(), //window width
+        h = $(window).height(); //window height
+
+    $(window).on('mousemove', function(e) {
+      var offsetX = 0.5 - e.pageX / w, //cursor position X
+        offsetY = 0.5 - e.pageY / h, //cursor position Y
+        dy = e.pageY - h / 2, //@h/2 = center of poster
+        dx = e.pageX - w / 2, //@w/2 = center of poster
+        theta = Math.atan2(dy, dx), //angle between cursor and center of poster in RAD
+        angle = theta * 180 / Math.PI - 90, //convert rad in degrees
+        offsetPoster = $poster.data('offset'),
+        transformPoster = 'translateY(' + -offsetX * offsetPoster + 'px) rotateX(' + (-offsetY * offsetPoster) + 'deg) rotateY(' + (offsetX * (offsetPoster * 2)) + 'deg)';
+
+      //get angle between 0-360
+      if (angle < 0) {
+        angle = angle + 360;
+      }
+
+      $poster.css('transform', transformPoster);
+
+  //parallax foreach layer
+    $layer.each(function() {
+        var $this = $(this),
+        offsetLayer = $this.data('offset') || 0,
+        transformLayer = 'translateX(' + offsetX * offsetLayer + 'px) translateY(' + offsetY * offsetLayer + 'px)';
+
+      $this.css('transform', transformLayer);
+    });
+    });
+
+    $(".login-button").click(function() {
+        $(".unavailable").css({'height':'170px','padding':'15px','border':'solid 1px rgb(255,72,0)'});
+    });
+
     $(".inline-header").click(function() {
         $(this).parent().toggleClass("inline-open");
         $('html, body').animate({
