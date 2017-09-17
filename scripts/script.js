1ea706f48792cfb905b60b66e685a844a1f18619
schultzy192 <schultzy192@gmail.com>
2017-09-17 18:39:48 +1200

    Added messages, and things

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 
+    $(".inline-header").click(function() {
+        $(this).parent().toggleClass("inline-open");
+        $(this).children(".material-icons").toggleClass("inline-arrow-spin");
+    });
+
+    var activeThread = document.getElementsByClassName("yarn-thread");
+    activeThread.scrollTop = activeThread.scrollHeight;
+
+    $(".sidebar-yarn").click(function() {
+        $(this).siblings().removeClass("active-yarn");
+        $(this).addClass("active-yarn");
+
+        if((this).hasClass("graphic-design")) {
+            $(".active-yarn").removeClass();
+            $(".active-yarn").addClass("graphic-design");
+        }
+
+        // if((this).hasClass("nsfw-flag")) {
+        //     $(".nsfw-warning").css("display","block");
+        // }
+    });
+
 });
